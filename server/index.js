6f28f522ce8629f14de1fbff2e065f40ae1b535d
Bhavyae Pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-10-02 09:26:15 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -5,11 +5,29 @@ const app=express();
 app.use(express.json())
 require("dotenv").config()
  
-app.use(  
-    cors({ 
-        origin:"*", 
-    })
-)
+// app.use(  
+//     cors({ 
+//         origin:"*", 
+//     })
+// )
+
+
+const allowedOrigins = ['https://todo-frontend-liart-alpha.vercel.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 
 const router=require("./routes/route")
 app.use("/api/v1",router)
